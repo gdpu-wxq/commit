7c6b69b36dc8d574a9c52819b5e0e3f4cb4f6d49
731554181@qq.com <731554181@qq.com>
2020-05-17 22:28:20 +0800

    更新 #hh#: gengxin

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,58 +1,52 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux";
-import { setInput } from "../store/actions/a"
-import { bindActionCreators } from 'redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { setInput } from '../store/actions/a'
+import { bindActionCreators } from 'redux'
 
 class AddTodo extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            data: '-----djsdhj0000000---'
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: '-----djsdhj0000000---',
     }
-    componentDidMount() {
+  }
+  componentDidMount() {}
+  componentWillReceiveProps() {
+    let aaa = this.props.haha
+    this.setState({ data: aaa })
+  }
+  go2User() {
+    this.props.history.push({
+      pathname: `./UserInfoPage`,
+      search: `?kkk=123`,
+      state: {
+        taskStatus: 2,
+      },
+    })
+  }
+  changed() {
+    let a = this.props.setInput('222kk22222')
+    console.log(a)
+  }
 
-    }
-    componentWillReceiveProps(){
-        let aaa = this.props.haha
-        this.setState({ data: aaa })
-    }
-    go2User() {
-        this.props.history.push({
-            pathname: `./UserInfoPage`,
-            search: `?kkk=123`,
-            state:{
-              taskStatus:2
-            }
-          })
-    }
-    changed(){
-        let a = this.props.setInput('222kk22222')
-        console.log(a)
-    }
-    
-    render() {
-        return (
-            <div>
-                <div onClick={this.go2User.bind(this)}>
-                    {this.props.haha}
-                </div>
-                <div onClick={this.changed.bind(this)}>
-                    change
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div onClick={this.go2User.bind(this)}>{this.props.haha}</div>
+        <div onClick={this.changed.bind(this)}>change</div>
+      </div>
+    )
+  }
 }
-const mapStateToProps = state => {
-    console.log(state)
-    return {
-        haha:state.a.inputValue
-    }
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    haha: state.a.inputValue,
+  }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    return { setInput: bindActionCreators(setInput, dispatch) }
+  return { setInput: bindActionCreators(setInput, dispatch) }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddTodo);
+export default connect(mapStateToProps, mapDispatchToProps)(AddTodo)
